5764b05bff808b6ce6f50ce66ff192f6ea027d5f
Barak Achillah Asidi <bachillah@gmail.com>
2022-10-17 10:39:45 +0300

    update workflow

--- tests/test_my_first_dag.py
+++ tests/test_my_first_dag.py
@@ -1,8 +1,11 @@
 import pytest
-from pytest_docker_tools import build
-from airflow.api.client.local_client import Client
+from airflow.models import DagBag
 
+@pytest.fixture
+def test_dag():
+    dag_id = "my_first_dag"
+    dag = DagBag().get_dag(dag_id)
+    return dag
 
-def test_my_first_dag():
-    c = Client(None, None)
-    c.trigger_dag(dag_id='my_first_dag', run_id='t1', conf={})
+def test_task_count(test_dag):
+    assert test_dag.tasks == 4
