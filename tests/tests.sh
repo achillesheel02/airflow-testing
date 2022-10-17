1a77fd2173c2582a4201edf5ce9930d96eee1b3f
Barak Achillah Asidi <bachillah@gmail.com>
2022-10-17 14:51:40 +0300

    update workflow

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 pip install apache-airflow pytest
+airflow connections add postgres \
+          --conn-type postgres \
+          --conn-host postgres \
+          --conn-port 5432 \
+          --conn-login postgres \
+          --conn-password postgres 
+
+airflow connections add user_data \
+          --conn-type http \
+          --conn-host  https://random-data-api.com/api/v2/ 
+
 pytest -rs
