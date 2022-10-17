d0332527437d36e579189fb7fdd13024539369d0
Barak Achillah Asidi <bachillah@gmail.com>
2022-10-17 10:26:12 +0300

    update workflow

--- tests/test_my_first_dag.py
+++ tests/test_my_first_dag.py
@@ -1,2 +1,8 @@
 import pytest
 from pytest_docker_tools import build
+from airflow.api.client.local_client import Client
+
+
+def test_my_first_dag():
+    c = Client(None, None)
+    c.trigger_dag(dag_id='my_first_dag', run_id='t1', conf={})
